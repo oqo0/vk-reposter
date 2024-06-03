88b78c999b188665708ba1bd18f4433859248441
oqo0 <oq.roman@gmail.com>
2024-06-03 21:35:05 +0300

    Implement vk to telegram reposter service

---
+++ VkToTelegramReposter/Services/VkToTelegramReposterService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Hosting;
+using Telegram.Bot;
+using VkTelegramReposter.Configuration;
+using VkTelegramReposter.Vk;
+
+namespace VkTelegramReposter.Services;
+
+public class VkToTelegramReposterService(
+    TelegramBotClient telegramBotClient,
+    Config config) : IHostedService
+{
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        foreach (var reposter in config.Reposters)
+        {
+            var vkGroupClient = new VkGroupClient(
+                reposter.VkGroupId,
+                reposter.VkGroupToken,
+                TimeSpan.FromSeconds(config.CheckCooldown));
+            
+            vkGroupClient.OnNewGroupPost += HandleNewPost;
+            vkGroupClient.StartListening(cancellationToken);
+        }
+        
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    private void HandleNewPost(ulong groupId, string newPostText, string[] images)
+    {
+        string telegramMessageText = newPostText + "\n" + string.Join("\n", images);;
+        
+        telegramBotClient.SendTextMessageAsync(
+            chatId: config.Reposters.First(r => r.VkGroupId == groupId).TelegramChannelId,
+            text: telegramMessageText
+        );
+    }
+}
