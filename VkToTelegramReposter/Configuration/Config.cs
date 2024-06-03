8f96750187e32a9a14e259916cb9e173f71aa8aa
oqo0 <oq.roman@gmail.com>
2024-06-03 21:34:24 +0300

    Add default configuration classes

---
+++ VkToTelegramReposter/Configuration/Config.cs
@@ -0,0 +1,18 @@
+﻿using YamlDotNet.Serialization;
+
+namespace VkTelegramReposter.Configuration;
+
+public class Config
+{
+    [YamlMember(typeof(string), Alias = "RepostWith")]
+    public string RepostWith { get; set; } = string.Empty;
+
+    [YamlMember(typeof(string), Alias = "TelegramBotToken")]
+    public string TelegramBotToken { get; set; } = string.Empty;
+
+    [YamlMember(typeof(int), Alias = "CheckCooldown")]
+    public int CheckCooldown { get; set;  }
+    
+    [YamlMember(typeof(List<Reposter>), Alias = "Reposters")]
+    public IList<Reposter> Reposters { get; set; } = new List<Reposter>();
+}
---
+++ VkToTelegramReposter/Configuration/Reposter.cs
@@ -0,0 +1,15 @@
+﻿using YamlDotNet.Serialization;
+
+namespace VkTelegramReposter.Configuration;
+
+public class Reposter
+{
+    [YamlMember(typeof(ulong), Alias = "VkGroupId")]
+    public ulong VkGroupId { get; set; }
+    
+    [YamlMember(typeof(string), Alias = "VkGroupToken")]
+    public string VkGroupToken { get; set; } = string.Empty;
+    
+    [YamlMember(typeof(string), Alias = "TelegramChannelId")]
+    public string TelegramChannelId { get; set; } = string.Empty;
+}
