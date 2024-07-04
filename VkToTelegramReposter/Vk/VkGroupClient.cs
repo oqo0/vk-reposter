6ecc967f4de9ee2dd71de9e2a1471ad86c355bc4
oqo0 <oq.roman@gmail.com>
2024-07-04 10:39:37 +0300

    Add server reconnection on failure

--- VkToTelegramReposter/Vk/VkGroupClient.cs
+++ VkToTelegramReposter/Vk/VkGroupClient.cs
@@ -3,125 +3,128 @@ using VkNet;
 using VkNet.Model;
 using VkTelegramReposter.Vk.Entities;
 
-namespace VkTelegramReposter.Vk;
-
-public class VkGroupClient
+namespace VkTelegramReposter.Vk
 {
-    public readonly ulong GroupId;
-    private readonly VkApi _vkApi = new();
-    private readonly TimeSpan _checkCooldownDelay;
-    private LongPollUrl _longPollUrl = null!;
-    
-    /// <summary>
-    /// Parameters: group id, post message, images
-    /// </summary>
-    public event Func<ulong, ulong, string, long, string[], Task>? OnNewGroupPost;
-
-    public VkGroupClient(ulong groupId, string groupPrivateToken)
-        : this(groupId, groupPrivateToken, TimeSpan.FromSeconds(3)) { }
-
-    public VkGroupClient(ulong groupId, string groupPrivateToken, TimeSpan checkCooldownDelay)
+    public class VkGroupClient
     {
-        var authParams = new ApiAuthParams
+        public readonly ulong GroupId;
+        private readonly VkApi _vkApi = new();
+        private readonly TimeSpan _checkCooldownDelay;
+        private LongPollUrl _longPollUrl = null!;
+
+        /// <summary>
+        /// Parameters: group id, post id, post message, group id, images
+        /// </summary>
+        public event Func<ulong, ulong, string, long, string[], Task>? OnNewGroupPost;
+
+        public VkGroupClient(ulong groupId, string groupPrivateToken)
+            : this(groupId, groupPrivateToken, TimeSpan.FromSeconds(3)) { }
+
+        public VkGroupClient(ulong groupId, string groupPrivateToken, TimeSpan checkCooldownDelay)
         {
-            AccessToken = groupPrivateToken
-        };
-        _vkApi.Authorize(authParams);
-
-        UpdateLongPollServer();
-        
-        GroupId = groupId;
-        _checkCooldownDelay = checkCooldownDelay;
-    }
-
-    public async Task StartListeningAsync(CancellationToken cancellationToken)
-    {
-        using var httpClient = new HttpClient();
-        httpClient.Timeout = TimeSpan.FromMinutes(2);
-
-        while (!cancellationToken.IsCancellationRequested)
-        {
-            await Task.Delay(_checkCooldownDelay, cancellationToken);
-
-            var updatesCheckResult = await CheckForUpdates(httpClient);
-            if (!updatesCheckResult)
-                return;
-        }
-    }
-
-    private void UpdateLongPollServer()
-    {
-        var response = _vkApi.Groups.GetLongPollServer(GroupId);
-        _longPollUrl = new LongPollUrl(response.Server, response.Key, response.Ts);
-    }
-    
-    private async Task<bool> CheckForUpdates(HttpClient httpClient)
-    {
-        var response = "None";
-        
-        try
-        {
-            response = await httpClient.GetStringAsync(_longPollUrl.Form());
-            return CheckResponseForUpdates(response);
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Exception in CheckForUpdates for group {GroupId}: {ex.Message} \n" +
-                              $"Response: {response}");
-            return false;
-        }
-    }
-
-    private bool CheckResponseForUpdates(string response)
-    {
-        var vkResponse = JsonSerializer.Deserialize<LongPollResponse>(response);
-
-        if (vkResponse == null)
-            return false;
-
-        if (vkResponse.Failed != null)
-        {
-            if (vkResponse.Failed == 1)
-                _longPollUrl.TimeStamp = Convert.ToUInt32(vkResponse.Ts);
-            else if (vkResponse.Failed is 2 or 3)
-                UpdateLongPollServer();
-
-            return true;
-        }
-            
-        foreach (var update in vkResponse.Updates)
-        {
-            ProcessUpdate(update);
-        }
-
-        _longPollUrl.TimeStamp = Convert.ToUInt32(vkResponse.Ts);
-        return true;
-    }
-
-    private void ProcessUpdate(Update update)
-    {
-        if (update.Type != "wall_post_new" || update.Object == null)
-            return;
-
-        var post = update.Object;
-        
-        if (post.PostType != "post")
-            return;
-        
-        var attachmentUrlList = new List<string>();
-        
-        foreach (var attachment in post.Attachments)
-        {
-            if (attachment.Photo == null)
-                continue;
-
-            var photoUrl = attachment.Photo.Sizes.LastOrDefault()?.Url;
-            if (!string.IsNullOrEmpty(photoUrl))
+            var authParams = new ApiAuthParams
             {
-                attachmentUrlList.Add(photoUrl);
+                AccessToken = groupPrivateToken
+            };
+            _vkApi.Authorize(authParams);
+
+            UpdateLongPollServer();
+
+            GroupId = groupId;
+            _checkCooldownDelay = checkCooldownDelay;
+        }
+
+        public async Task StartListeningAsync(CancellationToken cancellationToken)
+        {
+            using var httpClient = new HttpClient();
+            httpClient.Timeout = TimeSpan.FromMinutes(2);
+
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                await Task.Delay(_checkCooldownDelay, cancellationToken);
+
+                var updatesCheckResult = await CheckForUpdates(httpClient);
+                if (updatesCheckResult)
+                    continue;
+                
+                UpdateLongPollServer();
             }
         }
 
-        OnNewGroupPost?.Invoke(GroupId, post.Id, post.Text, post.OwnerId, attachmentUrlList.ToArray());
+        private void UpdateLongPollServer()
+        {
+            var response = _vkApi.Groups.GetLongPollServer(GroupId);
+            _longPollUrl = new LongPollUrl(response.Server, response.Key, response.Ts);
+        }
+
+        private async Task<bool> CheckForUpdates(HttpClient httpClient)
+        {
+            var response = "None";
+
+            try
+            {
+                response = await httpClient.GetStringAsync(_longPollUrl.Form());
+                return CheckResponseForUpdates(response);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception in CheckForUpdates for group {GroupId}: {ex.Message} \n" +
+                                  $"Response: {response}");
+                return false;
+            }
+        }
+
+        private bool CheckResponseForUpdates(string response)
+        {
+            var vkResponse = JsonSerializer.Deserialize<LongPollResponse>(response);
+
+            if (vkResponse == null)
+                return false;
+
+            if (vkResponse.Failed != null)
+            {
+                if (vkResponse.Failed == 1)
+                    _longPollUrl.TimeStamp = Convert.ToUInt32(vkResponse.Ts);
+                else if (vkResponse.Failed is 2 or 3)
+                    UpdateLongPollServer();
+
+                return true;
+            }
+
+            foreach (var update in vkResponse.Updates)
+            {
+                ProcessUpdate(update);
+            }
+
+            _longPollUrl.TimeStamp = Convert.ToUInt32(vkResponse.Ts);
+            return true;
+        }
+
+        private void ProcessUpdate(Update update)
+        {
+            if (update.Type != "wall_post_new" || update.Object == null)
+                return;
+
+            var post = update.Object;
+
+            if (post.PostType != "post")
+                return;
+
+            var attachmentUrlList = new List<string>();
+
+            foreach (var attachment in post.Attachments)
+            {
+                if (attachment.Photo == null)
+                    continue;
+
+                var photoUrl = attachment.Photo.Sizes.LastOrDefault()?.Url;
+                if (!string.IsNullOrEmpty(photoUrl))
+                {
+                    attachmentUrlList.Add(photoUrl);
+                }
+            }
+
+            OnNewGroupPost?.Invoke(GroupId, post.Id, post.Text, post.OwnerId, attachmentUrlList.ToArray());
+        }
     }
 }
